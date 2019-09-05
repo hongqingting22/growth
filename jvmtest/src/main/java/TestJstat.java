29eb0e86cd767cba80b7a0fb051ee53575542892
hongqingting22 <lixl_email@163.com>
2019-09-05 23:06:29 +0800

    jstat测试

---
+++ jvmtest/src/main/java/TestJstat.java
@@ -0,0 +1,43 @@
+public class TestJstat {
+
+    /**
+     * -XX:NewSize=104857600 -XX:MaxNewSize=104857600
+     * -XX:InitialHeapSize=209715200 -XX:MaxHeapSize=209715200
+     * -XX:PretenureSizeThreshold=3145728
+     * -XX:MaxTenuringThreshold=15
+     * -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:gc2.log
+     * -XX:+UseParNewGC -XX:+UseConcMarkSweepGC
+     * -XX:SurvivorRatio=8
+     *
+     * jstat可以分析出
+     * 1.每秒产生的对象大小
+     * 2.YGC频率
+     * 3.YGC后多少对象进入Sur0
+     * 4.
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args)  throws Exception{
+        Thread.sleep(30000);
+        while (true){
+            loadData();
+        }
+
+    }
+
+    /**
+     * 每秒钟产生50个100K的数据，且无引用（模拟每秒钟产生5M数据）
+     *
+     * Eden区80M  ，每秒5M，15秒后，yGC
+     * 之后都是15秒YGC
+     * @throws Exception
+     */
+    public static void loadData() throws Exception{
+        byte[] data = null;
+        for(int i = 1; i<=50;i++){
+            data = new byte[100 * 1024];
+        }
+        data = null;
+        Thread.sleep(1000);
+    }
+}
--- jvmtest/src/main/java/ToOldTest.java
+++ jvmtest/src/main/java/ToOldTest.java
@@ -1,5 +1,13 @@
 public class ToOldTest {
 
+    /**
+     * -XX:NewSize=10485760 -XX:MaxNewSize=10485760
+     * -XX:InitialHeapSize=20971520 -XX:MaxHeapSize=20971520
+     * -XX:PretenureSizeThreshold=10485760
+     * -XX:+UseParNewGC -XX:+UseConcMarkSweepGC
+     * -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:gc1.log
+     * @param args
+     */
     public static void main(String[] args) {
         byte[] bytes = new byte[2*1024 * 1024];
         bytes = new byte[2*1024*1024];
