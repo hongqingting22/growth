5ed69a0b6c38fa3cf66cacddc0680b127c9dc1e5
hongqingting22 <lixl_email@163.com>
2019-09-08 22:13:51 +0800

    lock

---
+++ problem-demo/src/main/java/com/lxl/lock/LockDemo.java
@@ -0,0 +1,13 @@
+package com.lxl.lock;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockDemo {
+
+    public static void main(String[] args) {
+//        AbstractQueuedSynchronizer
+
+        ReentrantLock
+    }
+}
