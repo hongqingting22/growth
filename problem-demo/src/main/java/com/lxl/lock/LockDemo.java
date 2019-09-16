e6d10ef1df3a706f342b8ab87b1f628d136c0edf
hongqingting22 <lixl_email@163.com>
2019-09-16 22:29:27 +0800

    AQS

--- problem-demo/src/main/java/com/lxl/lock/LockDemo.java
+++ problem-demo/src/main/java/com/lxl/lock/LockDemo.java
@@ -6,7 +6,7 @@ import java.util.concurrent.locks.ReentrantLock;
 public class LockDemo {
 
     public static void main(String[] args) {
-//        AbstractQueuedSynchronizer
+        AbstractQueuedSynchronizer
 
 //        ReentrantLock
     }
