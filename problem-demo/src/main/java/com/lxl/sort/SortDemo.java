78476ff0c433dda72be832a0cf78cde818390cd5
hongqingting22 <lixl_email@163.com>
2019-09-24 22:06:17 +0800

    插入排序

--- problem-demo/src/main/java/com/lxl/sort/SortDemo.java
+++ problem-demo/src/main/java/com/lxl/sort/SortDemo.java
@@ -48,10 +48,31 @@ public class SortDemo {
         }
     }
 
+    /**
+     * 插入排序
+     * @param arr
+     */
+    public static void insertionSort(int[] arr){
+        if(null != arr){
+            int length = arr.length;
+            int preIndex,current;
+            for(int i = 1;i<length;i++){
+                preIndex = i - 1;
+                current = arr[i];
+                while (preIndex >= 0 && arr[preIndex] > current){
+                    arr[preIndex + 1] = arr[preIndex];
+                    preIndex--;
+                }
+                arr[preIndex + 1] = current;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[]{2,1,3,5,4};
 //        bubbleSort(arr);
-        selectionSort(arr);
+//        selectionSort(arr);
+        insertionSort(arr);
         int length = arr.length;
         for(int i = 0;i < length;i++){
             System.out.println(arr[i]);
