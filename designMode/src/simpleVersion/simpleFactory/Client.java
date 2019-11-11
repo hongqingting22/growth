2d06e4088007dc25c24d08c1ce5132b28aa68439
hongqingting22 <lixl_email@163.com>
2019-11-11 22:35:34 +0800

    '简单工厂-多个方法，多个静态方法实现'

--- designMode/src/simpleVersion/simpleFactory/Client.java
+++ designMode/src/simpleVersion/simpleFactory/Client.java
@@ -5,6 +5,9 @@ public class Client {
 
     public static void main(String[] args) {
         Food food = null;
-        food = StaticFactory.getFood(args[0]);
+        food = StaticFactory.getFood("fruit");
+
+        //多个静态方法实现
+        food = StaticFactory.getFruit();
     }
 }
--- designMode/src/simpleVersion/simpleFactory/StaticFactory.java
+++ designMode/src/simpleVersion/simpleFactory/StaticFactory.java
@@ -3,6 +3,11 @@ package simpleVersion.simpleFactory;
 public class StaticFactory {
     private StaticFactory(){}
 
+    /**
+     * 简单工厂1-静态方法  if-else实现
+     * @param name
+     * @return
+     */
     public static Food getFood(String name){
         if(name.equals("Fruit")){
             return new Fruit();
@@ -11,4 +16,29 @@ public class StaticFactory {
         }
         return null;
     }
+
+    /**
+     * 简单工厂2-多个方法 实现
+     * @return
+     */
+/*    public Food getFruit(){
+        return new Fruit();
+    }
+
+    public Food getRice(){
+        return new Rice();
+    }*/
+
+    /**
+     * 简单工厂3-多个静态方法实现
+     */
+    public static Food getFruit(){
+        return new Fruit();
+    }
+
+    public static Food getRice(){
+        return new Rice();
+    }
+
+
 }
