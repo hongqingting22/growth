ce0190af2d1b0c47f09c2e1c110c2d1bc83f6558
hongqingting22 <lixl_email@163.com>
2020-04-10 10:11:16 +0800

    '链表反转，循环链表，约瑟夫问题'

---
+++ data_structure/src/main/java/com/lxl/linear/JosephLink.java
@@ -0,0 +1,75 @@
+package com.lxl.linear;
+
+/**
+ * 约瑟夫问题
+ *      使用循环链表
+ */
+public class JosephLink {
+
+    public static void main(String[] args) {
+        //构建约瑟夫环
+        Node<Integer> first = null;
+        Node<Integer> pre = null;
+        Node<Integer> node = null;
+        for(int i = 1;i<=41;i++){
+            //第一个节点
+            if(i == 1){
+                first = new Node(Integer.valueOf(i), null);
+                pre = first;
+            }else if(i == 41){
+                //最后一个节点
+                node = new Node(Integer.valueOf(i), first);
+                pre.next = node;
+            }else {
+                //其他节点
+                node = new Node(Integer.valueOf(i), null);
+                pre.next = node;
+                pre = node;
+            }
+
+        }
+
+        Node<Integer> yueSefu = first;
+//        while (yueSefu != null){
+//            yueSefu = yueSefu.next;
+//            System.out.println(yueSefu.item);
+//        }
+
+        print(yueSefu, 3);
+    }
+
+    public static void print(Node<Integer> node,int jump){
+        int count = 0;
+        Node<Integer> pre = null;
+        Node<Integer> cur = node;
+        Node<Integer> next;
+        while (cur != cur.next){
+            //=3时，前一节点的next指向下一节点；！=3时，前一节点向后移动到当前节点
+            if(++count == jump){
+                count = 0;
+                //当前节点移除
+                //找到下一个节点
+                next = cur.next;
+                //当前节点的next指向null
+//                cur.next = null;
+                //前一节点的next指向下一节点
+                pre.next = next;
+                System.out.println(cur.item);
+            }else{
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        System.out.println(cur.item);
+    }
+
+    static class Node<T> {
+        T item;
+        Node next;
+
+        public Node(T item, Node next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+}
---
+++ data_structure/src/main/java/com/lxl/linear/LinkListUseSlowFastPointer.java
@@ -0,0 +1,101 @@
+package com.lxl.linear;
+
+/**
+ * 快慢指针
+ *      可以解决的问题：
+ *          1.找到中间节点
+ *          2.判断链表是否有环
+ *          3.找到环的入口
+ */
+public class LinkListUseSlowFastPointer {
+
+    public static void main(String[] args) {
+        Node first = new Node(1,null);
+        Node second = new Node(2,null);
+        Node third = new Node(3,null);
+        Node four = new Node(4,null);
+        first.next = second;
+        second.next = third;
+        third.next = four;
+
+        Integer mid = getMid(first);
+        System.out.println(mid);
+
+        System.out.println(hasCircle(first));
+
+        four.next=second;
+        System.out.println(hasCircle(first));
+
+        System.out.println(getEntrance(first));
+    }
+
+    /**
+     * 查找中间节点
+     * @param node
+     * @return
+     */
+    public static Integer getMid(Node<Integer> node){
+        Node<Integer> slow = node;
+        Node<Integer> fast = node;
+        while (fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.item;
+    }
+
+    /**
+     * 判断链表是否有环
+     * @param node
+     * @return
+     */
+    public static boolean hasCircle(Node<Integer> node){
+        Node<Integer> slow = node;
+        Node<Integer> fast = node;
+        while (fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow.equals(fast))return true;
+        }
+        return false;
+    }
+
+    /**
+     * 找到环的入口
+     * @param node
+     * @return
+     */
+    public static Integer getEntrance(Node<Integer> node){
+        Node<Integer> slow = node;
+        Node<Integer> fast = node;
+        Node<Integer> temp = null;
+        while (fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow.equals(fast)){
+                temp = node;
+                continue;
+            }
+
+            if(temp != null){
+                temp = temp.next;
+                if(slow.equals(temp))
+                    break;
+            }
+        }
+        return temp.item;
+    }
+
+
+    static class Node<T> {
+        T item;
+        Node next;
+
+        public Node(T item, Node next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+}
